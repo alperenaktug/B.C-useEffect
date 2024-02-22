135181c62b4311efc8f8f15115f2320b10f2fc7c
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-22 12:11:50 +0300

    useEffect

---
+++ src/MyComponent.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
+
+  useEffect(() => {
+    document.title = `Count: ${count} ${color}`;
+  }, [count, color]);
+
+  function addCount() {
+    setCount((c) => c + 1);
+  }
+
+  function substractCount() {
+    setCount((c) => c - 1);
+  }
+  function changeColor() {
+    setColor((c) => (c === "green" ? "red" : "green"));
+  }
+
+  return (
+    <>
+      <p style={{ color: color }}>Count : {count}</p>
+      <button onClick={addCount}>Add</button>
+      <button onClick={substractCount}>Subtract</button>
+      <button onClick={changeColor}>Change Color</button>
+    </>
+  );
+}
+
+export default MyComponent;
